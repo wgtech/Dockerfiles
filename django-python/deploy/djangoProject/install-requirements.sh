711c6635ee49bbfc2a729869e1f85dbdc8fa3496
WangGi Lim <wanggilim@gmail.com>
2019-03-06 12:43:46 +0900

    pip setuptools upgrade added, fast-deploy dockerfile is developing

---
+++ django-python/deploy/djangoProject/install-requirements.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# pip requirements for your django app
+pip install numpy
+pip install pandas
---
+++ django-python/deploy/djangoProject/start-django.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# migrate and runserver
+for i in $(ls -d ./*/);
+do
+    python ${i%%/manage.py} migrate;
+    python ${i%%/manage.py} runserver;
+done
